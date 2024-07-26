a0a394c16dff171fb09c2941272d880ce3c1bbac
Zecrim <samuel.thomas424@gmail.com>
2024-07-25 20:11:25 -0500

    Fixed type error in Footer component by adding explicit type for props

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import styles from './Footer.module.css';
 
-const Footer = (props) => {
+const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
